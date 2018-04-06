30de6cf072f76416598db0a7af239ab74ae9994c
KamZet <37073291+KamZet@users.noreply.github.com>
2018-04-06 15:35:30 +0200

    Add files via upload

---
+++ PROI-project-2/src/Customer.cpp
@@ -0,0 +1,70 @@
+/*
+ * Customer.cpp
+ *
+ * Class Customer representing the theater client
+ * ^DEFINITION
+ *
+ * PROI, Lab project 2: 'Theater'
+ * Tutor: dr inz. Wiktor Kusmirek
+ * Version: 06.04.2018, Kamil Zacharczuk
+ */
+ 
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+
+#include "../include/Customer.hpp"
+#include "../include/Queue.hpp"
+
+/****CLASS CUSTOMER****/
+
+/*CONSTRUCTORS*/
+
+//// Parameters
+Customer::Customer(std::string p_forename, std::string p_surname, UNSH p_age){
+      this->forename = p_forename;
+      this->surname = p_surname;
+	this->age = p_age;
+}
+
+//// Default
+Customer::Customer()
+{
+      std::srand(std::time(nullptr));
+
+      this->forename = "Jan";
+      this->surname = "Kowalski";
+      this->age = rand()%77+13;
+}
+
+/*DESTRUCTOR*/
+Customer::~Customer() {}
+
+
+/*Operator == overloading*/
+bool Customer::operator== (Customer & another) const{
+	return
+		this->forename==another.forename &&
+		this->surname==another.surname &&
+		this->age==another.age;
+}
+
+/*METHODS*/
+
+//// Precised info
+void Customer::displayInfo (CUS_INFO info) const{
+      switch (info){
+      case FORE: std::cout << forename; break;
+      case SUR: std::cout << surname; break;
+      case AGE: std::cout << age; break;
+      default: std::cout << "unprecised info"; break;
+      }
+}
+
+//// All info
+void Customer::displayInfo() const{
+      std::cout << "| Imie: "; displayInfo(FORE);
+      std::cout << "| Nazwisko: "; displayInfo(SUR);
+      std::cout << "| Wiek: "; displayInfo(AGE);
+      std::cout << "|" << std::endl;
+}
---
+++ PROI-project-2/src/Menus.cpp
@@ -0,0 +1,84 @@
+/*
+ * Menus.cpp
+ *
+ * Class Menus created to handle travelling between menus in the test2 programme
+ * ^DEFINITION
+ *
+ * Lab project 2: 'Theater'
+ * Tutor: dr inz. Wiktor Kusmirek
+ * Version 06.04.18, Kamil Zacharczuk 
+ */
+ 
+#include <iostream>
+#include "../include/Menus.hpp"
+#include "../include/Customer.hpp"
+#include "../include/Show.hpp"
+#include "../include/Queue.hpp"
+
+/*Cons&des*/
+Menus::Menus() {}
+Menus::~Menus() {}
+
+//Cls
+void Menus::scroll(int n) const{
+	for (int i=0; i<n; i++){
+		std::cout << std::endl;
+	}
+}
+
+//Main menu
+UNSH Menus::main() const{
+      std::cout << "---MENU GLOWNE---" << std::endl;
+      std::cout << "1. Zarzadzaj klientami" << std::endl;
+      std::cout << "2. Zarzadzaj przedstawieniami" << std::endl;
+	  std::cout << "3. Zarzadzaj rezerwacjami" << std::endl;
+      std::cout << "0. Wyjdz z programu" << std::endl;
+
+      UNSH dec;
+      std::cin >> dec;
+
+      return dec;
+}
+
+//Customer
+UNSH Menus::cust() {
+            UNSH dec;
+
+            std::cout << "-ZARZADZANIE KLIENTAMI-" << std::endl;
+            std::cout << "1. Nowy klient" << std::endl;
+            std::cout << "2. Usun klienta" << std::endl;
+            std::cout << "3. Wyswietl liste klientow teatru" << std::endl;
+            std::cout << "0. Powrot do menu glownego" << std::endl;
+
+            std::cin >> dec;
+
+			return dec;
+}
+
+//Performance
+UNSH Menus::perf(){
+      UNSH dec;
+            std::cout << "- ZARZADZANIE PRZEDSTAWIENIAMI -" << std::endl;
+            std::cout << "1. Nowe przedstawienie" << std::endl;
+            std::cout << "2. Usun przedstawienie" << std::endl;
+            std::cout << "3. Wyswietl liste przedstawien" << std::endl;
+            std::cout << "0. Powrot do menu glownego" << std::endl;
+
+            std::cin >> dec;
+
+			return dec;
+}
+
+//Subscriptions
+UNSH Menus::sign(){
+	UNSH dec;
+		std::cout << "- ZARZADZANIE REZERWACJAMI -" << std::endl;
+		std::cout << "1. Zapisywanie" << std::endl
+			<< "2. Wypisywanie" << std::endl
+			<< "0. Powrot do meun glownego" << std::endl;
+
+		std::cin >> dec;
+
+		return dec;
+}
+
